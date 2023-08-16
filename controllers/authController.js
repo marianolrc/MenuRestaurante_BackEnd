a611b240fc6d5465bca5696518321e720484b72d
code_master <francojiseespinoza@gmail.com>
2023-08-16 17:53:55 -0300

    Jason Web Token Funcionando en las rutas protegidas

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,13 +9,13 @@ const login = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const user = await User.findOne({ email: email }).then((data) => res.json(data));
+        const user = await User.findOne({ email: email });
 
         if (!user || user.password !== password) {
             return res.status(401).json({ error: 'Invalid credentials' });
         }
 
-        const token = jwt.sign({ userId: user._id }, secretKey);
+        const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h'});
         res.json({ token });
     } catch (err) {
         response.status(500).json({ error: err.message });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,19 +5,19 @@ const userController = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 // Ruta protegida por autorizacion
-router.get('/users', userController.getAllUsers);
+router.get('/users', authMiddleware, userController.getAllUsers);
 
 // Ruta protegida para mostrar un usuario
-router.get('/user/:id', userController.getUser);
+router.get('/user/:id', authMiddleware, userController.getUser);
 
 // Ruta protegida para crear un nuevo usuario
-router.post('/users', userController.createUser);
+router.post('/users',  userController.createUser);
 
 // Ruta protegida para modificar algun usuario
-router.put('/user/:id', userController.updateUser);
+router.put('/user/:id', authMiddleware, userController.updateUser);
 
 // Ruta protegida para eliminar a un usuario
-router.delete('/user/:id' , userController.deleteUser);
+router.delete('/user/:id', authMiddleware, userController.deleteUser);
 
 // Aquí puedes agregar más rutas para obtener, actualizar o eliminar usuarios.
 
